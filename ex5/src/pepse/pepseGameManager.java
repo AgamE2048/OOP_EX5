7dd14d53bc407320768312c4a08f382141481e51
agam_e <agam.ebel@mail.huji.ac.il>
2022-12-30 12:00:05 +0200

    Start of work

--- ex5/src/pepse/pepseGameManager.java
+++ ex5/src/pepse/pepseGameManager.java
@@ -1,4 +1,24 @@
 package pepse;
 
-public class pepseGameManager {
+import danogl.GameManager;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import pepse.world.Sky;
+
+public class pepseGameManager extends GameManager {
+
+    public static void main(String[] args){
+        new pepseGameManager().run();
+    }
+
+    /*
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Sky.create(gameObjects(), windowController.getWindowDimensions(), Layer.BACKGROUND);
+    }
+     */
 }
