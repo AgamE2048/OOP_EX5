cf5fb6a74487b55e47d8af50aa5c6074fdfc268f
yuvi027 <yuvali027@gmail.com>
2023-01-04 18:34:47 +0200

    Started work on Avatar

--- ex5/src/pepse/world/Avatar.java
+++ ex5/src/pepse/world/Avatar.java
@@ -1,4 +1,17 @@
 package pepse.world;
 
+import danogl.collisions.GameObjectCollection;
+import danogl.gui.ImageReader;
+import danogl.gui.UserInputListener;
+import danogl.util.Vector2;
+
 public class Avatar {
+
+    public static Avatar create(GameObjectCollection gameObjects,
+                                int layer, Vector2 topLeftCorner,
+                                UserInputListener inputListener,
+                                ImageReader imageReader){
+
+        return null;
+    }
 }
