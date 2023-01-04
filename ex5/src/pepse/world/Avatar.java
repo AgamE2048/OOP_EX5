a08ba37669decf50a331330ae0a562f347324f8c
agam_e <agam.ebel@mail.huji.ac.il>
2023-01-04 22:40:16 +0200

    Avatar update -- now  gameObject :)

--- ex5/src/pepse/world/Avatar.java
+++ ex5/src/pepse/world/Avatar.java
@@ -12,7 +12,7 @@ import danogl.util.Vector2;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
-public class Avatar{
+public class Avatar extends GameObject{
     private static final int MOVE_SPEED = 300;
     private static final int JUMP_VELOCITY = -300;
     private static final int ACCELERATION_FROM_SKY  = 500;
@@ -21,13 +21,26 @@ public class Avatar{
     private static float energy;
     private UserInputListener inputListener;
 
+    /**
+     * Construct a new GameObject instance.
+     *
+     * @param topLeftCorner Position of the object, in window coordinates (pixels).
+     *                      Note that (0,0) is the top-left corner of the window.
+     * @param dimensions    Width and height in window coordinates.
+     * @param renderable    The renderable representing the object. Can be null, in which case
+     *                      the GameObject will not be rendered.
+     */
+    public Avatar(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
+        super(topLeftCorner, dimensions, renderable);
+    }
+
     public static Avatar create(GameObjectCollection gameObjects,
                                 int layer, Vector2 topLeftCorner,
                                 UserInputListener inputListener,
                                 ImageReader imageReader){
 
         Renderable avatarRenderable = new RectangleRenderable(Color.black);
-        GameObject avatar = new GameObject(topLeftCorner, new Vector2(100, 100),
+        Avatar avatar = new Avatar(topLeftCorner, new Vector2(100, 100),
                 avatarRenderable);
         avatar.renderer().setIsFlippedHorizontally(true);
 
@@ -39,7 +52,19 @@ public class Avatar{
     }
 
 
-    public void update(GameObject avatar) {
+    /**
+     * Should be called once per frame.
+     *
+     * @param deltaTime The time elapsed, in seconds, since the last frame. Can
+     *                  be used to determine a new position/velocity by multiplying
+     *                  this delta with the velocity/acceleration respectively
+     *                  and adding to the position/velocity:
+     *                  velocity += deltaTime*acceleration
+     *                  pos += deltaTime*velocity
+     */
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
         Vector2 movementDir = Vector2.ZERO;
         if(inputListener.isKeyPressed(KeyEvent.VK_LEFT)){
             movementDir = movementDir.add(Vector2.LEFT);
@@ -55,9 +80,8 @@ public class Avatar{
                 movementDir = new Vector2(movementDir.x(), JUMP_VELOCITY);
             }
             else{
-                avatar.transform().setAccelerationY(ACCELERATION_FROM_SKY);
+                this.transform().setAccelerationY(ACCELERATION_FROM_SKY);
             }
-
         }
 
         if(inputListener.isKeyPressed(KeyEvent.VK_SPACE)){
@@ -68,6 +92,8 @@ public class Avatar{
 
         //TODO: add option to stay in place
 
-        avatar.setVelocity(movementDir.mult(MOVE_SPEED));
+        this.setVelocity(movementDir.mult(MOVE_SPEED));
     }
 }
+
+
