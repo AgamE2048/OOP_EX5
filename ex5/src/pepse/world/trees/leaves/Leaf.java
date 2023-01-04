a15dbbe0461ec5f514f261d4eeb1ad20f1aecd42
yuvi027 <yuvali027@gmail.com>
2023-01-04 17:31:16 +0200

    Working on falling leaves

--- ex5/src/pepse/world/trees/leaves/Leaf.java
+++ ex5/src/pepse/world/trees/leaves/Leaf.java
@@ -15,14 +15,21 @@ import java.util.Random;
 public class Leaf {
     private static final Color BASE_COLOR_LEAVES = new Color(50, 200, 30);
     private GameObject leaf;
+    private int lifeTime;
+    private int fadeTime;
 
     private GameObjectCollection gameObjects;
+    private Vector2 topLeftCorner;
 
     public Leaf(GameObjectCollection gameObjects){
         this.gameObjects = gameObjects;
+        Random rand = new Random();
+        this.lifeTime = rand.nextInt(30) + 5;
+        this.fadeTime = rand.nextInt(5) + 5;
     }
 
     public GameObject create(Vector2 topLeftCorner){
+        this.topLeftCorner = topLeftCorner;
         GameObject leaf = new GameObject(topLeftCorner, Vector2.ONES.mult(Block.SIZE), new
                 RectangleRenderable(ColorSupplier.approximateColor(BASE_COLOR_LEAVES)));
         this.leaf = leaf;
@@ -30,6 +37,8 @@ public class Leaf {
         leaf.setTag("leaf"); //TODO: add proper layer
 
         new ScheduledTask(leaf, (float)randX(10, 1), true, this::run);
+        new ScheduledTask(leaf, this.lifeTime, true, this::falling);
+//        new ScheduledTask(leaf, this.lifeTime + this.fadeTime, true, this::releaf);
 
         return leaf;
     }
@@ -39,7 +48,7 @@ public class Leaf {
         return (int) (rand.nextDouble() * strech + bound);
     }
 
-    public void run(){
+    private void run(){
         float angle = randX(10, 10);
         float size = randX(5,3);
 
@@ -54,5 +63,15 @@ public class Leaf {
                 Vector2.ONES.mult(Block.SIZE - size), Vector2.ONES.mult(Block.SIZE + size),
                 Transition.LINEAR_INTERPOLATOR_VECTOR, 7,
                 Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null);
+    }
+
+    private void falling(){
+        leaf.transform().setVelocityY(70);
+        leaf.renderer().fadeOut(this.fadeTime, () -> releaf());
+        this.gameObjects.removeGameObject(leaf);
+    }
+
+    private void releaf(){
+        create(topLeftCorner);
     }
 }
